3655c1a1f2e3c10770499cfb0a25a4286e0497e0
Kevin C. Escobedo <kevincescobedo@gmail.com>
2025-02-11 17:23:50 -0800

    Started Paddle class

---
+++ Paddle.hpp
@@ -0,0 +1,21 @@
+#ifndef PADDLE_HPP
+#define PADDLE_HPP
+
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <random>
+
+class Paddle: public sf::RectangleShape
+{
+public:
+    Paddle(float x, float y, float width, float height);
+    Paddle(const sf::Vector2f& position, const sf::Vector2f& dimensions);
+    Paddle(const Paddle& paddle);
+    Paddle& operator =(const Paddle& paddle);
+
+private:
+    static sf::Color randomColor();
+};
+
+#endif /* PADDLE_HPP */
